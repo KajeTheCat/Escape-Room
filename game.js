6a440b586b501b2c085b1f46a372b5ea1370108d
Jacob <jamsbury95@hotmail.com>
2021-11-20 13:03:11 -0800

    fixed transitions and all

--- game.js
+++ game.js
@@ -1,32 +1,54 @@
 'use strict';
 
-
 let symbol = document.querySelector('#symbols');
 let pyramidOne = document.querySelector('#pyramidOne');
 let pyramidTwo = document.querySelector('#pyramidTwo');
 let pyramidThree = document.querySelector('#pyramidThree');
 let pyramidFour = document.querySelector('#pyramidFour');
+let tbody = document.querySelector('tbody');
+let victorsDiv = document.querySelector('.victorsDiv');
+let loserDiv = document.querySelector('.loserDiv');
+let userName = '';
+let userTimeScore = '';
+let time = '';
+let gamePlayerArr = JSON.parse(localStorage.getItem('victors')) || [];
+
+function GamePlayer(name, userTimeScore) {
+  this.name = name;
+  this.userTimeScore = userTimeScore;
+  gamePlayerArr.push(this);
+}
+
+function saveGameFile() {
+  let stringifiedArr = JSON.stringify(gamePlayerArr);
+  localStorage.setItem('victors', stringifiedArr);
+}
+
+function instantiateGamePlayer(name,userTimeScore) {
+  new GamePlayer(name,userTimeScore);
+  saveGameFile();
+}
 
 let questionDb = [
   {
     question: 'In reply to an inquiry about the animals on his farm, the farmer says: “I only ever keep sheep, goats, and horses. In fact, at the moment they are all sheep bar three, all goats bar four, and all horses bar five.” How many goats does he have?'
-    , answer: 2
+    , answer: '2'
   }
   , {
     question: 'If a hen and a half lay an egg and a half in a day and a half, how many eggs will half a dozen hens lay in half a dozen days?'
-    , answer: 24
+    , answer: '24'
   }
   , {
     question: 'My twin lives at the reverse of my house number. The difference between our house numbers ends in two. What are the lowest possible numbers of our house numbers?'
-    , answer: 72
+    , answer: '72'
   }
   , {
     question: 'I have a calculator that can display ten digits. How many different ten-digit numbers can I type using just the 0-9 keys once each, and moving from one keypress to the next using the knight’s move in chess?'
-    , answer: 4
+    , answer: '4'
   }
   , {
     question: 'A grandfather, two fathers and two sons went to the movie theater together and everyone bought one movie ticket each. How many tickets did they buy in total?'
-    , answer: 3
+    , answer: '3'
   }
   , {
     question: 'I’m tall when I’m young, and I’m short when I’m old. What am I?'
@@ -62,10 +84,32 @@ let questionDb = [
   }
 ];
 
+function renderVictors() {
+  for (let i=0;i<gamePlayerArr.length;i++) {
+    let tr = document.createElement('tr');
+    let tdName = document.createElement('td');
+    tdName.textContent = gamePlayerArr[i].name;
+    let tdTime = document.createElement('td');
+    tdTime.textContent = gamePlayerArr[i].userTimeScore;
+    tr.append(tdName, tdTime);
+    tbody.appendChild(tr);
+  }
+}
+
+function victory() {
+  alert('you have collected the pharoh’s treasure!');
+  userTimeScore = time;
+  userName = prompt('What is thy name?');
+  instantiateGamePlayer(userName, userTimeScore);
+  clearInterval(ticker);
+  renderVictors();
+  victorsDiv.style.display = 'flex';
+}
+
 function renderGreenLight() {
   if (correctAnswerCounter == 4) {
     pyramidFour.style.color = '#34eb7a';
-    alert('you have collected the pharoh’s treasure!');
+    victory();
   } else if (correctAnswerCounter == 3) {
     pyramidThree.style.color = '#34eb7a';
   } else if (correctAnswerCounter == 2) {
@@ -74,13 +118,15 @@ function renderGreenLight() {
     pyramidOne.style.color = '#34eb7a';
   }
 }
+
 let correctAnswerCounter = 0;
 
 function symbolPuzzle(event) {
   let randomNum = Math.floor(Math.random() * questionDb.length);
   let randomQuestion = questionDb[randomNum].question;
   let symbolPuzzleAnswer = prompt(randomQuestion);
-  if (symbolPuzzleAnswer == questionDb[randomNum].answer) {
+  let symbolPuzzleCorrectAnswer = questionDb[randomNum].answer;
+  if (symbolPuzzleAnswer.toLowerCase() === symbolPuzzleCorrectAnswer.toLowerCase()) {
     correctAnswerCounter++;
     renderGreenLight();
     if (correctAnswerCounter >= 4) {
@@ -149,7 +195,7 @@ function startTimer(secs) {
 }
 
 function gameOver() {
-  alert('Game Over');
+  loserDiv.style.display = 'flex';
 }
 
 function tick() {
@@ -161,14 +207,13 @@ function tick() {
   }
   let mins = Math.floor(secs / 60);
   secs %= 60;
-  let time = ((mins < 10) ? '0' : '') + mins + ':' + ((secs < 10) ? '0' : '') + secs;
+  time = ((mins < 10) ? '0' : '') + mins + ':' + ((secs < 10) ? '0' : '') + secs;
   if (time.toString() === '00:00') {
     gameOver();
   }
   let countdown = document.querySelector('#countdown');
   countdown.textContent = time;
 }
-
 
 function renderSymbols() {
   let img = document.querySelector('#symbols');
@@ -180,9 +225,4 @@ function renderSymbols() {
   img.style.left = xPos + 'px';
 }
 
-
-
-
 startTimer(600);
-
-
